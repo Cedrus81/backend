16218452e270a2eff5025fc79499f5676a9c0d5f
Cedrus81 <erez.eitan.97@gmail.com>
2022-12-17 16:15:36 +0200

    built in the frontend

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName : 'tester_db'
+  dbURL: `mongodb+srv://erez:${process.env.DB_PASSWORD}@cluster0.9ggajqn.mongodb.net/?retryWrites=true&w=majority`,
+  dbName: 'task_db'
 }
